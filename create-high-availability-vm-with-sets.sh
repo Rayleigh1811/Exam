c61a209a7f2877aefe38d94b10e0cd7fd0e6a02f
Rayleigh1811 <131571607+Rayleigh1811@users.noreply.github.com>
2023-08-02 05:23:53 +0700

    Update create-high-availability-vm-with-sets.sh

--- create-high-availability-vm-with-sets.sh
+++ create-high-availability-vm-with-sets.sh
@@ -39,10 +39,10 @@ az network nsg rule create \
 # Create the NIC
 for i in `seq 1 3`; do
   echo '------------------------------------------'
-  echo 'Creating Phi1811'$i
+  echo 'Creating webNic'$i
   az network nic create \
     --resource-group $RgName \
-    --name Phi1811$i \
+    --name phiNic$i \
     --vnet-name bePortalVnet \
     --subnet bePortalSubnet \
     --network-security-group bePortalNSG
@@ -60,8 +60,8 @@ for i in `seq 1 3`; do
     az vm create \
         --admin-username azureuser \
         --resource-group $RgName \
-        --name PhiVM$i \
-        --nics Phi1811$i \
+        --name phiVM$i \
+        --nics phiNic$i \
         --image UbuntuLTS \
         --availability-set portalAvailabilitySet \
         --generate-ssh-keys \
